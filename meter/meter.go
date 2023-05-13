438ba686c415fd79728f0cfd97ebfb31b4c19755
carlosms <d1videsb1zer0@gmail.com>
2023-05-13 23:02:15 +0100

    add burner method

--- meter/meter.go
+++ meter/meter.go
@@ -37,6 +37,7 @@ func main() {
 	account := common.HexToAddress("0x18e2CeE48035F4558Eb75a629C37d713EFC005c2")
 	readContract(account)
 	//recordProduction(account)
+	recordConsumption(account)
 	readContract(account)
 	//go produceCounter()
 	//go consumptionCounter()
@@ -151,6 +152,45 @@ func recordProduction(account common.Address) {
 	fmt.Printf("tx sent: %s", tx.Hash().Hex())
 }
 
-func recordConsumption() {
+func recordConsumption(account common.Address) {
+	wallet := LoadWallet()
+
+	client, err := ethclient.Dial("https://polygon-testnet-rpc.allthatnode.com:8545")
+	if err != nil {
+		// handle error
+		panic(err)
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), wallet.address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(wallet.privateKey, big.NewInt(80001))
+	if err != nil {
+		panic(err)
+	}
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	address := common.HexToAddress("0x3092ef862A180D0f44C5E537EfE05Cd7DCbB28A7")
+	instance, err := NewZap(address, client)
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := instance.BurnToken(auth, account, big.NewInt(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", tx.Hash().Hex())
 
 }
