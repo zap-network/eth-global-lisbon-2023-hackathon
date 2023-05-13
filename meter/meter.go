fdb2dc71f165940c03b949eacc5734cf71040c24
Gonçalo Pereira <goncalonsp@gmail.com>
2023-05-13 22:27:44 +0100

    Implemented zap production

--- meter/meter.go
+++ meter/meter.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"math/big"
 	"os"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/labstack/echo"
@@ -29,11 +33,14 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%+v", cfg)
-	go startEcho()
-	readContract()
-	go produceCounter()
-	go consumptionCounter()
-	select {}
+	//go startEcho()
+	account := common.HexToAddress("0x18e2CeE48035F4558Eb75a629C37d713EFC005c2")
+	readContract(account)
+	//recordProduction(account)
+	readContract(account)
+	//go produceCounter()
+	//go consumptionCounter()
+	//select {}
 }
 
 func initConfig() (config, error) {
@@ -81,12 +88,11 @@ func consumptionCounter() {
 	}
 }
 
-func readContract() {
+func readContract(account common.Address) {
 
 	client, err := ethclient.Dial("https://polygon-testnet-rpc.allthatnode.com:8545")
 	if err != nil {
 		// handle error
-
 	}
 
 	address := common.HexToAddress("0x3092ef862A180D0f44C5E537EfE05Cd7DCbB28A7")
@@ -95,11 +101,56 @@ func readContract() {
 		panic(err)
 	}
 
-	bal, err := instance.BalanceOf(nil, common.HexToAddress("0x18e2CeE48035F4558Eb75a629C37d713EFC005c2"))
+	bal, err := instance.BalanceOf(nil, account)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("\n\n balanceOf %+v \n\n", bal)
+
+}
+
+func recordProduction(account common.Address) {
+	wallet := LoadWallet()
+
+	client, err := ethclient.Dial("https://polygon-testnet-rpc.allthatnode.com:8545")
+	if err != nil {
+		// handle error
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), wallet.address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(wallet.privateKey, big.NewInt(80001))
+	if err != nil {
+		panic(err)
+	}
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	address := common.HexToAddress("0x3092ef862A180D0f44C5E537EfE05Cd7DCbB28A7")
+	instance, err := NewZap(address, client)
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := instance.IssueToken(auth, account, big.NewInt(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", tx.Hash().Hex())
+}
+
+func recordConsumption() {
 
 }
---
+++ meter/wallet.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type Wallet struct {
+	privateKey *ecdsa.PrivateKey
+	address    common.Address
+}
+
+func LoadWallet() Wallet {
+	privateKeyString := os.Getenv("WALLET_PKEY")
+	privateKey, err := crypto.HexToECDSA(privateKeyString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	return Wallet{
+		privateKey: privateKey,
+		address:    address,
+	}
+}
